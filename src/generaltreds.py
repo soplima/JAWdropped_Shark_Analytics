7fed80a0e9de1e500baa3735aa6b6979d88129a6
soplima <sop.lima13@gmail.com>
2025-05-02 14:37:09 -0300

    feat: general features were analized

--- src/generaltreds.py
+++ src/generaltreds.py
@@ -6,10 +6,11 @@ import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 import seaborn as sns
 import calendar
+from calendar import month_name
 #%%
 app = dash.Dash(__name__)
 #%%
-data = pd.read_csv('../../data/attacks.csv')
+data = pd.read_csv('../data/attacks.csv')
 # %%
 df = pd.DataFrame(data)
 # %%
@@ -274,23 +275,33 @@ atttackGenderRatio = (
 
 filtered_gender_ratio = atttackGenderRatio[atttackGenderRatio['Sex'] != 'Unknown']
 
-# Pivot for heatmap
-pivot_table = filtered_gender_ratio.pivot(index='Sex', columns='Decade', values='attack_count').fillna(0)
+# Create separate dataframes for male and female
+male_data = filtered_gender_ratio[filtered_gender_ratio['Sex'] == 'Male']
+female_data = filtered_gender_ratio[filtered_gender_ratio['Sex'] == 'Female']
 
-# Plotly Heatmap
-fig9 = go.Figure(data=go.Heatmap(
-    z=pivot_table.values,
-    x=pivot_table.columns,
-    y=pivot_table.index,
-    colorscale='Blues',
-    colorbar=dict(title='Attack Count')
+fig9 = go.Figure()
+
+fig9.add_trace(go.Scatter(
+    x=male_data['Decade'],
+    y=male_data['attack_count'],
+    mode='lines',
+    name='Male',
+    line=dict(color='cyan', width=3)
+))
+
+fig9.add_trace(go.Scatter(
+    x=female_data['Decade'],
+    y=female_data['attack_count'],
+    mode='lines',
+    name='Female',
+    line=dict(color='pink', width=3)
 ))
 
 fig9.update_layout(
     title='Victim Gender Ratio Over Decades',
     title_x=0.5,
     xaxis_title='Decade',
-    yaxis_title='Gender',
+    yaxis_title='Attack Count',
     template='plotly_dark',
     plot_bgcolor='rgb(18, 18, 18)',
     paper_bgcolor='rgb(18, 18, 18)',
@@ -346,6 +357,68 @@ fig10.update_layout(
 )
 
 fig10.show()
+
+#%%
+#* Do attacks from migratory species (e.g., bull sharks) spike during certain months?
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+df['Month'] = df['Date'].dt.month_name()
+attacksByMonth = df.groupby('Month').size().reset_index(name='Count')
+ordered_months = list(month_name[1:])
+attacksByMonth['Month'] = pd.Categorical(attacksByMonth['Month'], categories=ordered_months, ordered=True)
+attacksByMonth = attacksByMonth.sort_values('Month')
+
+filtered_df = df[
+    (df['Species'].notna()) &
+    (df['Month'].notna())
+]
+
+#Create combined group attack by species
+top10 = (
+    filtered_df[filtered_df['Species'] != 'Unknown Shark']
+    ['Species'].value_counts()
+    .head(10)
+    .index
+)
+filtered_df = filtered_df[filtered_df['Species'].isin(top10)]
+
+# Group by SpeciesGroup and Month
+species_group = (
+    filtered_df.groupby(['Species', 'Month'])
+    .size()
+    .reset_index(name='attack_count')
+)
+
+species_group['Month'] = pd.Categorical(species_group['Month'], categories=ordered_months, ordered=True)
+
+
+# Create pivot table for heatmap
+pivot_table_species = species_group.pivot(
+    index='Species',
+    columns='Month',
+    values='attack_count'
+).fillna(0)
+
+fig11 = go.Figure(data=go.Heatmap(
+    z=pivot_table_species.values,
+    x=pivot_table_species.columns,
+    y=pivot_table_species.index,
+    colorscale='Blues',
+    colorbar=dict(title='Attack Count')
+))
+
+fig11.update_layout(
+    title='Attacks by Species and Month',
+    title_x=0.5,
+    xaxis_title='Month',
+    yaxis_title='Species',
+    template='plotly_dark',
+    plot_bgcolor='rgb(18,18,18)',
+    paper_bgcolor='rgb(18,18,18)',
+    margin=dict(l=40, r=40, t=40, b=40)
+)
+
+fig11.show()
+
 #%%
 app.layout = html.Div(
     children=[
@@ -356,52 +429,81 @@ app.layout = html.Div(
             id='shark-attack-graph1',
             figure=fig
         ),
+        html.P("""Over the years, there has been a noticeable increase in the number of 
+               recorded shark attacks. This trend is likely influenced not only by actual 
+               changes in incident rates, but also by improved data collection and greater 
+               availability of recent records.""", style={'color': 'white', 'textAlign': 'center'}),
+
 
         # Second Graph
         dcc.Graph(
             id='shark-attack-graph2',
             figure=fig2
         ),
+        html.P("""Most shark attacks were recorded in the United States, likely because data collection 
+               efforts have been more concentrated there, making incident records more readily available.""", style={'color': 'white', 'textAlign': 'center'}),
         
         dcc.Graph(
             id='shark-attack-graph2',
             figure=fig3
         ),
+        html.P("""Shark attacks peak in June, July, and August, likely due to increased water activity 
+               during summer vacation in the United States. A smaller peak in January may be explained by summer holidays 
+               in Australia, the second country with the highest number of recorded attacks.""", style={'color': 'white', 'textAlign': 'center'}),
         
         dcc.Graph(
             id='shark-attack-graph2',
             figure=fig4
         ),
+        html.P("""Most shark attacks occurred during water-based activities such as swimming, 
+               surfing, or diving. Interestingly, the second-highest number of incidents involved people on boats, where sharks attacked the vessels rather than individuals directly in the water.""", style={'color': 'white', 'textAlign': 'center'}),
         
         dcc.Graph(
             id='shark-attack-graph2',
             figure=fig5
         ),
+        html.P("""The majority of shark attacks were on males, with a male-to-female ratio of approximately 8:1.""", style={'color': 'white', 'textAlign': 'center'}),
         
         dcc.Graph(
             id='shark-attack-graph2',
             figure=fig6
         ),
+        html.P("""The majority of shark attacks occurred in the afternoon, followed by the morning, likely due to 
+               these being the peak times for water-based activities.""", style={'color': 'white', 'textAlign': 'center'}),
         
         dcc.Graph(
             id='shark-attack-graph2',
             figure=fig7
         ),
+        html.P("""The location with the most recorded shark attacks is 
+               New Smyrna Beach, located in Volusia County, Florida, 
+               which is famously known as the 'Shark Bite Capital of the World.""", style={'color': 'white', 'textAlign': 'center'}),
         
         dcc.Graph(
             id='shark-attack-graph2',
             figure=fig8
         ),
+        html.P("""The distribution of shark attacks by age group reveals 
+               that adults are the most affected, followed by teenagers.""", style={'color': 'white', 'textAlign': 'center'}),
         
         dcc.Graph(
             id='shark-attack-graph2',
             figure=fig9
         ),
+        html.P(""""The distribution of shark attacks over the decades shows a consistent predominance of male victims compared to female victims, highlighting a clear gender disparity in incident occurrence.""", style={'color': 'white', 'textAlign': 'center'}),
         
         dcc.Graph(
             id='shark-attack-graph2',
             figure=fig10
-        )
+        ),
+        html.P(""""The distribution indicates that the majority of fatal shark attacks involve adult male victims.""", style={'color': 'white', 'textAlign': 'center'}),
+        
+        dcc.Graph(
+            id='shark-attack-graph2',
+            figure=fig11
+        ),
+        html.P(""""According to the distribution, most attacks are attributed to great white sharks and tend to occur in September. This aligns with their migratory behavior, as white sharks begin
+               moving south from their northern feeding grounds when water temperatures drop and food becomes scarce.""", style={'color': 'white', 'textAlign': 'center'}),
         
     ],
     style={'backgroundColor': 'rgb(18, 18, 18)', 'color': 'white', 'padding': '20px'}  # Dark background for the whole page
@@ -413,5 +515,6 @@ if __name__ == '__main__':
     print("Server is running on http://127.0.0.1:8050")
 
 
+
+
 # %%
-#* Do attacks from migratory species (e.g., bull sharks) spike during certain months?
